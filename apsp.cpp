e4dff072116e84110250c069e3e250807fe25695
ananyakumarijsr19-arch <ananyakumari.jsr19@gmail.com>
2026-03-11 08:11:47 +0530

    Add files via upload

---
+++ apsp.cpp
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define INF 9999
+
+int main() {
+    int n, i, j, k;
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    int dist[n][n];
+
+    printf("Enter adjacency matrix (use %d for no edge):\n", INF);
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            scanf("%d", &dist[i][j]);
+        }
+    }
+
+    for (k = 0; k < n; k++) {
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) {
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    printf("\nAll Pairs Shortest Path Matrix:\n");
+    for (i = 0; i < n; i++) {
+        for (j = 0; j < n; j++) {
+            if (dist[i][j] == INF)
+                printf("INF ");
+            else
+                printf("%d ", dist[i][j]);
+        }
+        printf("\n");
+    }
+
+    return 0;
+}  
